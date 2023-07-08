c3d4b249f7374eb3fe62499a6f113f5f0c7a44a1
Kanat <kuzebaevk@gmail.com>
2023-07-08 19:43:12 +0600

    Initial Commit

---
+++ AviasalesTest/AviasalesTestApp.swift
@@ -0,0 +1,17 @@
+//
+//  AviasalesTestApp.swift
+//  AviasalesTest
+//
+//  Created by Канат Кузебаев on 8/7/23.
+//
+
+import SwiftUI
+
+@main
+struct AviasalesTestApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
